755ca9d5709d57b735540d159d175bcc42f80043
samandralves <sam.andralves2018@gmail.com>
2026-09-01 11:50:30 -0300

    Update world.js

--- static/js/world.js
+++ static/js/world.js
@@ -43,7 +43,7 @@ const ELEMENT_SLOTS = {
     0: {
       url: "/static/img/house.png",
       baseWidth: 95,
-      offset: { x: 0, y: -37 },
+      offset: { x: 0, y: -48 },
       anchor: { x: 0.5, y: 1 },
       idle: "smoke",
     },
