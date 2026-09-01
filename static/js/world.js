b96085ec86d669449da12ff294a57260c739c86e
samandralves <sam.andralves2018@gmail.com>
2026-09-01 11:52:55 -0300

    Update world.js

--- static/js/world.js
+++ static/js/world.js
@@ -50,14 +50,14 @@ const ELEMENT_SLOTS = {
     30: {
       url: "/static/img/pond.png",
       baseWidth: 96,
-      offset: { x: -88, y: 18 },
+      offset: { x: -74, y: 10 },
       anchor: { x: 0.5, y: 0.62 },
       idle: "shimmer",
     },
     50: {
       url: "/static/img/bench_recortado.png",
       baseWidth: 48, // mais ~10% menor (era 53)
-      offset: { x: 58, y: 21 }, // mais perto da lagoa, fechando o vazio no meio da grama
+      offset: { x: 42, y: 14 }, // mais perto da lagoa, fechando o vazio no meio da grama
       anchor: { x: 0.5, y: 0.78 },
       idle: null,
     },
