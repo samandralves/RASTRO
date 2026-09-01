26dff0ea41b3ab3c973697cff1379fb7100b8216
samandralves <sam.andralves2018@gmail.com>
2026-09-01 09:50:55 -0300

    Update world.js

--- static/js/world.js
+++ static/js/world.js
@@ -43,7 +43,7 @@
     0: {
       url: "/static/img/house.png",
       baseWidth: 95,
-      offset: { x: -25, y: -60 },
+      offset: { x: -20, y: -60 },
       anchor: { x: 0.5, y: 1 },
       idle: "smoke",
     },
